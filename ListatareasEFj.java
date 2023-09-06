1ec995af3279300801416896171ddd1d2a18fcdf
Lalofuja <143622730+Lalofuja@users.noreply.github.com>
2023-09-06 08:48:40 -0600

    Add files via upload

---
+++ ListatareasEFj.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package variable2efj;
+
+/**
+ *
+ * @author Usuario
+ */
+public class ListatareasEFj {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String tarea1 = "hacer tarea ";
+        String tarea2 = "ir al gimnasio ";
+        String tarea3 = "tomar una ducha ";
+        String tarea4 = "preparar mi comida del dia siguiente ";
+        String tarea5 = "ir a dormir ";
+        
+        System.out.println("Hola soy eduardo y estas son mis tareas del dia a dia;"+ tarea1 + tarea2 + tarea3 + tarea4 + tarea5);
+        
+        
+   
+              
+        // TODO code application logic here
+    }
+    
+}
